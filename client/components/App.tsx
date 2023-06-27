971a72ecaf6cf8dc3bb38ac79b4ea3ba8046c800
scott.park1 <kcp951130@gmail.com>
2023-06-27 15:58:18 +1200

    Separate routes and css on footer

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
-import Standings from "./Standings"
-import Stats from "./Stats"
-import Teams from "./Teams"
-import TeamDetails from "./TeamDetails"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Standings from './Standings'
+import Stats from './Stats'
+import Teams from './Teams'
+import TeamDetails from './TeamDetails'
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Teams />} />
-          <Route path='/team/:id' element={<TeamDetails />} />
+          <Route path="/team" element={<Teams />} />
+          <Route path="/team/:id" element={<TeamDetails />} />
+          <Route path="/standings" element={<Standings />} />
+          <Route path="/stats" element={<Stats />} />
         </Routes>
-        <Standings />
-        <Stats />
         <Footer />
       </div>
     </Router>
