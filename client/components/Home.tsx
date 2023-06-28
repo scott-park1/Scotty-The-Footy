894998a381c46fdcf5947d3c6732994f22b24416
scott.park1 <kcp951130@gmail.com>
2023-06-28 23:51:47 +1200

    Add css styling on links in home page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div className="home">
-      <h1>&quot;WELCOME TO THE BEST FOOTY WEBSITE EVER.&quot;</h1>
+      <h2>&quot;WELCOME TO THE BEST FOOTY WEBSITE EVER.&quot;</h2>
       <div className="homeheading">
         <h3 className="homelist">
-          <Link to="/team">Teams</Link>
+          <Link to="/team" className="button">
+            Teams
+          </Link>
         </h3>
         <h3>Matches</h3>
         <h3 className="homelist">
-          <Link to="/standings">Standings</Link>
+          <Link to="/standings" className="button">
+            Standings
+          </Link>
         </h3>
         <h3 className="homelist">
-          <Link to="/stats">Stats</Link>
+          <Link to="/stats" className="button">
+            Stats
+          </Link>
         </h3>
       </div>
     </div>
