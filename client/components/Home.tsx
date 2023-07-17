9b322ee1500725fdce7b7d937c584f6f42709998
scott.park1 <kcp951130@gmail.com>
2023-07-17 14:18:36 +1200

    Add CSS

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div className="home">
-      <h2>&quot;WELCOME TO THE BEST FOOTY WEBSITE EVER.&quot;</h2>
       <div className="homeheading">
         <h3 className="homelist">
           <Link to="/team" className="button">
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h1>SCOTTY THE FOOTY ⚽</h1>
+      <h1>SCOTTY THE FOOTY</h1>
       <br></br>
     </div>
   )
